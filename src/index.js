2f20366f35454e8c44d2f037fba1662c8866a279
Elmer <elmeralbertoperez@gmail.com>
2024-09-21 10:45:37 -0600

    Conexion a base de datos

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ const express = require('express');
 // Inicializaciones
 const app = express();
 
+require('dotenv').config()
+
 // Ajustes del servidor
-app.set('port', process.env.PORT || 4000);
+app.set('port', process.env.PORT || 4500);
+
+app.use(require('./routes'));
 
 // Iniciar el servidor
 app.listen(app.get('port'), () => {
